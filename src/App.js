80d874749f6ca112814171297dd21bdc0b59a004
Mohammed <moh.jameel178@gmail.com>
2019-02-13 22:03:18 +0300

    Done!!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,167 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Meme from './Meme'
 
 class App extends Component {
   render() {
+
+    const memes = [
+      {
+        "title": "Angry",
+        "image": "http://imgur.com/kH6o0mN.png"
+      },
+      {
+        "title": "Aww Yeah",
+        "image": "http://imgur.com/vTSZ6CG.png"
+      },
+      {
+        "title": "Cereal Guy",
+        "image": "http://imgur.com/SjnMMhd.png"
+      },
+      {
+        "title": "Cereal Spitting",
+        "image": "http://imgur.com/rdw32yx.png"
+      },
+      {
+        "title": "Challenge Accepted",
+        "image": "http://imgur.com/xHcS2LF.png"
+      },
+      {
+        "title": "Heck Fuck Yea",
+        "image": "http://imgur.com/DRd0yKE.png"
+      },
+      {
+        "title": "Concentrated",
+        "image": "http://imgur.com/t9uwvai.png"
+      },
+      {
+        "title": "Derrrp",
+        "image": "http://imgur.com/kBSZgbG.png"
+      },
+      {
+        "title": "Determined",
+        "image": "http://imgur.com/CATjyHj.png"
+      },
+      {
+        "title": "Dude Come On",
+        "image": "http://imgur.com/8Tkr6ld.png"
+      },
+      {
+        "title": "EWBTE",
+        "image": "http://imgur.com/ZaSaYAK.png"
+      },
+      {
+        "title": "Excited Troll",
+        "image": "http://imgur.com/CFLPW9U.png"
+      },
+      {
+        "title": "Fap Fap",
+        "image": "http://imgur.com/NImikb7.png"
+      },
+      {
+        "title": "Forever Alone",
+        "image": "http://imgur.com/4L89AMP.png"
+      },
+      {
+        "title": "Forever Alone Excited",
+        "image": "http://imgur.com/NyEoz2g.png"
+      },
+      {
+        "title": "Oh Yea Female",
+        "image": "http://imgur.com/WMgvwaq.png"
+      },
+      {
+        "title": "Full Panel",
+        "image": "http://imgur.com/iN5P0r4.png"
+      },
+      {
+        "title": "Happy",
+        "image": "http://imgur.com/JqhcMU4.png"
+      },
+      {
+        "title": "Hehehe",
+        "image": "http://imgur.com/U9GK6jL.png"
+      },
+      {
+        "title": "Laughing",
+        "image": "http://imgur.com/wJUtNXR.png"
+      },
+      {
+        "title": "Me Gusta",
+        "image": "http://imgur.com/6otWkQZ.png"
+      },
+      {
+        "title": "Milk",
+        "image": "http://imgur.com/bxMhEKZ.png"
+      },
+      {
+        "title": "Newspaper Guy",
+        "image": "http://imgur.com/MhbPTSQ.png"
+      },
+      {
+        "title": "Okay",
+        "image": "http://imgur.com/X6niVYQ.png"
+      },
+      {
+        "title": "Original Rage",
+        "image": "http://imgur.com/CD0nLh8.png"
+      },
+      {
+        "title": "Original Troll",
+        "image": "http://imgur.com/sV4AReH.png"
+      },
+      {
+        "title": "Poker Face",
+        "image": "http://imgur.com/EFHsGkJ.png"
+      },
+      {
+        "title": "Prrrr",
+        "image": "http://imgur.com/gXmsig6.png"
+      },
+      {
+        "title": "Smile",
+        "image": "http://imgur.com/Bp2y4zT.png"
+      },
+      {
+        "title": "Thoughtful",
+        "image": "http://imgur.com/HdmBrwX.png"
+      },
+      {
+        "title": "Troll Dad Full",
+        "image": "http://imgur.com/Mgyz87x.png"
+      },
+      {
+        "title": "Troll Dad Jump",
+        "image": "http://imgur.com/PFzOfO7.png"
+      },
+      {
+        "title": "Wait A Minute",
+        "image": "http://imgur.com/rxlv8TX.png"
+      },
+      {
+        "title": "What",
+        "image": "http://imgur.com/0JmUzko.png"
+      },
+      {
+        "title": "Why With Hands",
+        "image": "http://imgur.com/gEZW60M.png"
+      },
+      {
+        "title": "YUNO",
+        "image": "http://imgur.com/uXKy3RJ.png"
+      }
+    ]
+
+
+    const memesList = memes.map((meme) => {
+      return (
+        <Meme meme={meme} />
+      );
+    });
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {memesList}
       </div>
     );
   }
---
+++ src/Meme.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+
+class Meme extends Component {
+    state = {
+        hide: 'false'
+    }
+    hideMeme = () => {
+        console.log('Hello')
+        if (this.state.hide === 'false') {
+            this.setState({ hide: 'hidden' })
+        } else {
+            this.setState({ hide: 'false' })
+        }
+    }
+    render() {
+
+        return (
+            <div>
+                <h1 onClick={this.hideMeme}>{this.props.meme.title}</h1>
+                <img className={this.state.hide} src={this.props.meme.image} />
+            </div>
+        )
+
+    }
+}
+
+export default Meme;
